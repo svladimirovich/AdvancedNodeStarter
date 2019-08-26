317a3a01d4b2e24eb3f51061ba35ee14c0ad8122
svladimirovich <mikser.vladimirovich@yandex.ru>
2019-08-26 23:23:34 +0300

    [my_changes] downloaded from github and set up my settings

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://mishima:nzb7o20khYs8C9nt@myatlas-shard-00-00-dcjmw.mongodb.net:27017,myatlas-shard-00-01-dcjmw.mongodb.net:27017,myatlas-shard-00-02-dcjmw.mongodb.net:27017/test?ssl=true&replicaSet=MyAtlas-shard-0&authSource=admin&retryWrites=true&w=majority',
   cookieKey: '123123123'
 };

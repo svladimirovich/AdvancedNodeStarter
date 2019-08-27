63d089e0c92fe1319b6c443f45720b5c1bbd3ab0
svladimirovich <mikser.vladimirovich@yandex.ru>
2019-08-27 23:56:42 +0300

    [my_changes] simple caching with redis example

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -14,9 +14,22 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
 
-    res.send(blogs);
+    const client = require('redis').createClient('redis://127.0.0.1:6379');
+    const util = require('util');
+    client.get = util.promisify(client.get);
+
+    const cachedBlogs = await client.get(req.user.id);
+
+    if(cachedBlogs) {
+      console.log("serving from CACHE");
+      res.send(JSON.parse(cachedBlogs));
+    } else {
+      console.log("serving from MongoDB");
+      const blogs = await Blog.find({ _user: req.user.id });
+      res.send(blogs);
+      client.set(req.user.id, JSON.stringify(blogs));
+    }
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {

280a3e38c5a4debd729f48d5fb7ed33458f6cf50
svladimirovich <mikser.vladimirovich@yandex.ru>
2019-08-29 23:39:35 +0300

    [my_changes] cache fragmented by users and clearCache() function implemented

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
+const { clearCache } = require('../services/cache');
 
 const Blog = mongoose.model('Blog');
 
@@ -14,7 +15,7 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id }).cache();
+    const blogs = await Blog.find({ _user: req.user.id }).cache({ key: req.user.id });
 
     res.send(blogs);
   });
@@ -33,6 +34,8 @@ module.exports = app => {
       res.send(blog);
     } catch (err) {
       res.send(400, err);
+    } finally {
+      clearCache(req.user.id);
     }
   });
 };
--- services/cache.js
+++ services/cache.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 const client = require('redis').createClient('redis://127.0.0.1:6379');
 const util = require('util');
-client.get = util.promisify(client.get);
+client.hget = util.promisify(client.hget);
 
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.cache = function() {
+mongoose.Query.prototype.cache = function(options = {}) {
     this.useCache = true;
+    this.hashKey = JSON.stringify(options.key || '');
     return this;
 }
 
@@ -17,7 +18,7 @@ mongoose.Query.prototype.exec = async function() {
         const keyObject = Object.assign({}, this.getQuery(), { collection: this.mongooseCollection.name });
         const key = JSON.stringify(keyObject);
     
-        const cacheValue = await client.get(key);
+        const cacheValue = await client.hget(this.hashKey, key);
     
         if (cacheValue) {
             console.log("From CACHE:", cacheValue);
@@ -30,9 +31,15 @@ mongoose.Query.prototype.exec = async function() {
     
         const result = await exec.apply(this, arguments);
         // expiration in redis set to 10 seconds
-        client.set(key, JSON.stringify(result), 'EX', 10);
+        client.hset(this.hashKey, key, JSON.stringify(result), 'EX', 10);
         return result;
     } else {
         return exec.apply(this, arguments);
+    }
+}
+
+module.exports = {
+    clearCache(key) {
+        client.del(JSON.stringify(key));
     }
 }

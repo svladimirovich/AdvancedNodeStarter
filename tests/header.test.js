b83a4fc8622cad04bee5da6274215521e4b9a1a1
svladimirovich <mikser.vladimirovich@yandex.ru>
2019-08-31 01:15:15 +0300

    [my_changes] simple integration test

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,4 +1,19 @@
+const puppeteer = require('puppeteer');
+
 test('Adds two numbers', () => {
     const sum = 3 + 5;
     expect(sum).toEqual(8);
 });
+
+test('We can launch a broswer', async () => {
+    const broswer = await puppeteer.launch({
+        headless: false
+    });
+    const page = await broswer.newPage();
+    await page.goto('localhost:3000');
+
+    const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+
+    expect(text).toEqual('Blogster');
+});
+

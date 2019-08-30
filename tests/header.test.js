8e91051fb421506a43a4ab460b71ad904205f943
svladimirovich <mikser.vladimirovich@yandex.ru>
2019-08-31 01:46:11 +0300

    [my_changes] browser start and close

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,19 +1,29 @@
 const puppeteer = require('puppeteer');
 
+let browser;
+let page;
+
+beforeAll(async () => {
+    browser = await puppeteer.launch({
+        headless: false
+    });
+    page = await browser.newPage();
+    await page.goto('localhost:3000');
+});
+
+afterAll(() => {
+    browser.close();
+});
+
 test('Adds two numbers', () => {
     const sum = 3 + 5;
     expect(sum).toEqual(8);
 });
 
 test('We can launch a broswer', async () => {
-    const broswer = await puppeteer.launch({
-        headless: false
-    });
-    const page = await broswer.newPage();
-    await page.goto('localhost:3000');
 
     const text = await page.$eval('a.brand-logo', el => el.innerHTML);
-
     expect(text).toEqual('Blogster');
+
 });
 

8f29a1431ce7b491d7514c0e8b573a4d9d769385
svladimirovich <mikser.vladimirovich@yandex.ru>
2019-08-31 02:01:47 +0300

    [my_changes] test for login link navigation

--- tests/header.test.js
+++ tests/header.test.js
@@ -20,10 +20,14 @@ test('Adds two numbers', () => {
     expect(sum).toEqual(8);
 });
 
-test('We can launch a broswer', async () => {
-
+test('The header has the correct text', async () => {
     const text = await page.$eval('a.brand-logo', el => el.innerHTML);
     expect(text).toEqual('Blogster');
+});
 
+test('clicking "login" starts oauth flow', async () => {
+    await page.click('.right a');
+    const url = await page.url();
+    expect(url).toMatch(/accounts\.google\.com/);
 });
 

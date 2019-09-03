9ada4f05d3ac11014168f86f9932f9edee6f09cd
svladimirovich <mikser.vladimirovich@yandex.ru>
2019-09-03 23:25:25 +0300

    [my_changes] extracted login method, and fixed browser.close()

--- tests/helpers/customPage.js
+++ tests/helpers/customPage.js
@@ -1,16 +1,39 @@
 const puppeteer = require('puppeteer');
+const userFactory = require('../factories/userFactory');
+const sessionFactory = require('../factories/sessionFactory');
 
 class CustomPage {
+
+    constructor(page, browser) {
+        this.page = page;
+        this.browser = browser;
+    }
+
     static async build() {
         const browser = await puppeteer.launch({ headless: false });
         const page = await browser.newPage();
-        const customPage = new CustomPage(page);
+        const customPage = new CustomPage(page, browser);
 
         return new Proxy(customPage, {
             get: function(target, property) {
                 return customPage[property] || browser[property] || page[property];
             }
         });
+    }
+
+    async login() {
+        const user = await userFactory();
+        const { session, sig } = sessionFactory(user);
+    
+        await this.page.setCookie( { name: "session", value: session });
+        await this.page.setCookie( { name: "session.sig", value: sig });
+        await this.page.goto('localhost:3000');
+        await this.page.waitFor('a[href="/auth/logout"]');
+    
+    }
+
+    async close() {
+        return this.browser.close();
     }
 }
 

f646c48c23ecf51affb152e062abe2368ec05289
svladimirovich <mikser.vladimirovich@yandex.ru>
2019-09-04 00:29:00 +0300

    [my_changes] jest timeout settings (commented out because they are not needed yet)

--- tests/setup.js
+++ tests/setup.js
@@ -1,3 +1,5 @@
+// jest.setTimeout(30000); // how long to wait before automatically failing a test (the default is 5 seconds (5000))
+
 const mongoose = require('mongoose')
 const keys = require('../config/keys');
 
